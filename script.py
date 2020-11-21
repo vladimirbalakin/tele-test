8448d26a656dac60caf9e3cdaf307ca7850b8b5a
Vladimir <balakinvladimir87@gmail.com>
2020-11-21 14:38:45 +0300

    commit

--- script.py
+++ script.py
@@ -6,3 +6,6 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(func = lambda message: True, content_types = [text])
 def answer(message):
     bot.send_message(message.chat_id, message.text)
+
+if __name__ == "__main__":
+    bot.polling(non_stop = True)
