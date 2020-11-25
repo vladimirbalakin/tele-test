c4ae6641e2c0fcb782dd737d67f23261c4a8eb32
Vladimir <balakinvladimir87@gmail.com>
2020-11-25 14:57:15 +0300

    commit

--- script.py
+++ script.py
@@ -1,11 +1,29 @@
 from config import BOT_TOKEN
+from random import randint
 import telebot
+import time
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(func = lambda message: True, content_types = [text])
-def answer(message):
-    bot.send_message(message.chat_id, message.text)
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row('Stone', 'Paper', 'Scissors')
+gameboard = ['Stone', 'Paper', 'Scissors']
 
-if __name__ == "__main__":
-    bot.polling(non_stop = True)
+@bot.message_handler(commands = ['start'])
+def startMess(message):
+    #print(message)
+    bot.send_message(message.chat.id, 'Hello, ' + message.chat.first_name + ' ' + message.chat.last_name + '!')
+@bot.message_handler(commands = ['new_game'])
+def newGame(message):
+    bot.send_message(message.chat.id, "Great. Choose your turn", reply_markup = keyboard)
+
+@bot.message_handler(commands = ['text'])
+def resived(message):
+    print(message)
+    if message in gameboard:
+        answer = randint(0, 2)
+        bot.send_message(message.chat.id, gameboard[answer])
+@bot.message_handler(content_types = ['sticker'])
+def sticker_id(message):
+    print(message)
+bot.polling()
