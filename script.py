15d8993d553473dd2cd67779a4e740a7b9c8eb96
Vladimir <balakinvladimir87@gmail.com>
2020-11-25 18:29:03 +0300

    main
    Answering to /new_game

--- script.py
+++ script.py
@@ -1,17 +1,29 @@
-from config import BOT_TOKEN
 from random import randint
-import telebot
 import time
+
+import telebot
+
+from config import BOT_TOKEN
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard.row('Stone', 'Paper', 'Scissors')
 gameboard = ['Stone', 'Paper', 'Scissors']
+winTable = {0 : 2, 1 : 0, 3 : 2}
+
+def setWin(numUser, numBot):
+    global winTable
+    if numUser == numBot:
+        return 'Draw'
+    if winTable[numUser] == numBot:
+        return 'User wins'
+    else:
+        return 'Bot wins'
 
 @bot.message_handler(commands = ['start'])
 def startMess(message):
-    #print(message)
+    # print(message)
     bot.send_message(message.chat.id, 'Hello, ' + message.chat.first_name + ' ' + message.chat.last_name + '!')
 @bot.message_handler(commands = ['new_game'])
 def newGame(message):
@@ -19,11 +31,15 @@ def newGame(message):
 
 @bot.message_handler(content_types = ['text'])
 def resived(message):
-    #print(message)
+    # print(message)
     if message.text in gameboard:
-        answer = randint(0, 2)
+        answer = randint(0, len(gameboard) - 1)
         bot.send_message(message.chat.id, gameboard[answer])
+        numUser = gameboard.index(message.text)
+        bot.send_message(message.chat.id, setWin(numUser, answer))
 @bot.message_handler(content_types = ['sticker'])
 def sticker_id(message):
     print(message)
 bot.polling()
+#gitignore.io
+#PEP-8
